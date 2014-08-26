57bfa808077e508972c5914e92d22305cb4e9407
Miko <Miko@ValaskalaPC>
2014-08-26 16:55:08 +0300

    Project start! :D

---
+++ Boardgame Project (Suurin Kirja)/src/com/infinimango/game/GameState.java
@@ -0,0 +1,21 @@
+package com.infinimango.game;
+
+import java.awt.Graphics;
+
+import com.infinimango.flux.State;
+
+public class GameState extends State {
+	
+	public GameState() {
+		// TODO Initialize state
+	}
+	
+	public void update() {
+		// TODO Update state
+	}
+	
+	public void render(Graphics g) {
+		// TODO Render state
+	}
+	
+}
