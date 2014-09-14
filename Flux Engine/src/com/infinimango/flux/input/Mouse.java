6200cf0babd1bdec1755792eb2564c35c6f1ab0c
InfiniMango <infinimango@gmail.com>
2014-09-14 22:53:26 +0300

    Mouse button crash fix

--- Flux Engine/src/com/infinimango/flux/input/Mouse.java
+++ Flux Engine/src/com/infinimango/flux/input/Mouse.java
@@ -1,7 +1,9 @@
 package com.infinimango.flux.input;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.MouseInfo;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 public class Mouse extends MouseAdapter {
 	// Mouse button constants
@@ -9,7 +11,8 @@ public class Mouse extends MouseAdapter {
 	public static final int MIDDLE = 2;
 	public static final int RIGHT = 3;
 	// State of all mouse buttons
-	private static boolean buttons[] = new boolean[MouseInfo.getNumberOfButtons()];
+	private static boolean buttons[] = new boolean[MouseInfo
+			.getNumberOfButtons() + 1];
 	// Indicates if the mouse is currently on the screen
 	private static boolean onScreen = false;
 	// Coordinates of the mouse
@@ -23,100 +26,117 @@ public class Mouse extends MouseAdapter {
 
 	/**
 	 * Initializes the mouse adapter.
-	 * @param scale Pixel scaling of the game for realistic coordinates
+	 * 
+	 * @param scale
+	 *            Pixel scaling of the game for realistic coordinates
 	 */
-	public Mouse(int scale){
+	public Mouse(int scale) {
 		Mouse.scale = scale;
 	}
 
+	@Override
 	public void mousePressed(MouseEvent e) {
 		buttons[e.getButton()] = true;
 		updateLocation(e);
 	}
 
+	@Override
 	public void mouseReleased(MouseEvent e) {
 		buttons[e.getButton()] = false;
 		updateLocation(e);
 	}
 
+	@Override
 	public void mouseEntered(MouseEvent e) {
 		onScreen = true;
 		updateLocation(e);
 	}
 
+	@Override
 	public void mouseExited(MouseEvent e) {
 		onScreen = false;
 		updateLocation(e);
 	}
 
+	@Override
 	public void mouseDragged(MouseEvent e) {
 		updateLocation(e);
 	}
 
+	@Override
 	public void mouseMoved(MouseEvent e) {
 		updateLocation(e);
 	}
 
+	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
 		wheelRotation += e.getWheelRotation();
 	}
 
 	/**
 	 * Updates the mouse coordinates.
-	 * @param e MouseEvent which includes the new coordinates
+	 * 
+	 * @param e
+	 *            MouseEvent which includes the new coordinates
 	 */
-	private void updateLocation(MouseEvent e){
+	private void updateLocation(MouseEvent e) {
 		x = e.getX() / scale;
 		y = e.getY() / scale;
 	}
 
 	/**
 	 * Get mouses horizontal location on the screen
+	 * 
 	 * @return Mouse x-coordinate
 	 */
-	public static int getX(){
+	public static int getX() {
 		return x;
 	}
 
 	/**
 	 * Get mouses vertical location on the screen
+	 * 
 	 * @return Mouse y-coordinate
 	 */
-	public static int getY(){
+	public static int getY() {
 		return y;
 	}
 
 	/**
 	 * Checks if a mouse button is currently down.
-	 * @param id Which button to check for
+	 * 
+	 * @param id
+	 *            Which button to check for
 	 * @return True, if the button is down
 	 */
-	public static boolean buttonDown(int id){
+	public static boolean buttonDown(int id) {
 		return buttons[id];
 	}
 
 	/**
 	 * Checks if the cursor is on the screen.
+	 * 
 	 * @return True, if cursor is within the screen bounds
 	 */
-	public static boolean isOnScreen(){
+	public static boolean isOnScreen() {
 		return onScreen;
 	}
 
 	/**
-	 * Returns the current wheel rotation. Note! Wheel rotation does not
-	 * reset itself, so you must use a variable to determine the change for scrolling,
+	 * Returns the current wheel rotation. Note! Wheel rotation does not reset
+	 * itself, so you must use a variable to determine the change for scrolling,
 	 * or use the reset()-method to reset mouse wheel(and buttons).
+	 * 
 	 * @return Current rotation of the wheel
 	 */
-	public static int getWheelRotation(){
+	public static int getWheelRotation() {
 		return wheelRotation;
 	}
 
 	/**
 	 * Resets button states back to false and sets wheel rotation back to 0.
 	 */
-	public static void reset(){
+	public static void reset() {
 		for (int i = 0; i < buttons.length; i++) {
 			buttons[i] = false;
 		}
